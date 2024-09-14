69ad813a485995db2bb1470516db8f1cb48fb2af
nalim07 <alimdulah@gmail.com>
2024-09-14 15:43:47 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+// function
+let data1 = true;  
+let data2 = false;
+
+function tes(data1, data2) {
+    
+    if (data1 == true) {
+        data1 = "Berhasil"
+    } else {
+        data1 = "Gagal"
+    }
+
+    console.log(data1);
+}   
+tes(data1, data2);
+
+// Perhitungan
+let a = 10;
+let b = 10;
+
+function hitung(a, b) {
+    return a + b;
+}
+
+console.log(hitung(a, b));  
+
